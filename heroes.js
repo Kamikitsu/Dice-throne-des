8ac17bd9740130a4f84adbe4452167a0b3a44b90
Kamikitsu <lucasgauvin@gmail.com>
2026-09-14 21:38:26 +0200

    Update heroes.js

--- heroes.js
+++ heroes.js
@@ -5,34 +5,6 @@
 
 window.HEROES = {
 
-  generic: {
-    name: "Dés génériques",
-    defaultDiceCount: 5,
-    info: "Version de test.",
-    faces: [
-      { value: 1, symbol: "Symbole A" },
-      { value: 2, symbol: "Symbole A" },
-      { value: 3, symbol: "Symbole B" },
-      { value: 4, symbol: "Symbole B" },
-      { value: 5, symbol: "Symbole C" },
-      { value: 6, symbol: "Symbole D" }
-    ]
-  },
-
-  hero6: {
-    name: "Héros à 6 dés (exemple)",
-    defaultDiceCount: 6,
-    info: "Exemple d'un personnage dont le nombre de dés par défaut est 6.",
-    faces: [
-      { value: 1, symbol: "Symbole A" },
-      { value: 2, symbol: "Symbole A" },
-      { value: 3, symbol: "Symbole B" },
-      { value: 4, symbol: "Symbole B" },
-      { value: 5, symbol: "Symbole C" },
-      { value: 6, symbol: "Symbole D" }
-    ]
-  },
-
  spiderman: {
   name: "Spider-Man",
   defaultDiceCount: 5,
